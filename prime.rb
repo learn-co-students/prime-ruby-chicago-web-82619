d643480b236c28960c0c5539801c2c3110f6dcd2
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-08-29 12:42:06 -0500

    lab complete

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  
+  factor = 2
+  while factor <= (num/factor.to_f)
+    if num % factor == 0 
+      return false
+    end
+    factor += 1
+  end
+  true 
+end
